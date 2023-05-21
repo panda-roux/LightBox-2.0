dbce85745c7e662c0ad9a4acd35be9f55b16cac9
panda-roux <serras.nick@gmail.com>
2023-05-21 15:37:10 +0100

    Workin Pre-Alpha 0.6

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,30 @@ void setup() {
     Serial.begin(9600);
     
     // Setup the MCP23016 expanders
-    mcpManager.setupExpander(MCP23016_ADDRESS_1);
-    mcpManager.setupExpander(MCP23016_ADDRESS_2);
-    mcpManager.setupExpander(MCP23016_ADDRESS_3);
-    mcpManager.setupExpander(MCP23016_ADDRESS_4);
+    if (!mcpManager.setupExpander(MCP23016_ADDRESS_1)) {
+        Serial.println("Failed to setup expander at address 1");
+    }
+    if (!mcpManager.setupExpander(MCP23016_ADDRESS_2)) {
+        Serial.println("Failed to setup expander at address 2");
+    }
+    if (!mcpManager.setupExpander(MCP23016_ADDRESS_3)) {
+        Serial.println("Failed to setup expander at address 3");
+    }
+    if (!mcpManager.setupExpander(MCP23016_ADDRESS_4)) {
+        Serial.println("Failed to setup expander at address 4");
+    }
 }
 
 void loop() {
     // Read all inputs
     mcpManager.readAllInputs();
 
-    // Print the state of all inputs
-    for (int i = 0; i < MAX_EXPANDERS * 16; i++) {
-        Serial.print(i);
-        Serial.print(": ");
-        Serial.print(mcpManager.getInputState(i));
-        Serial.print("   ");
+    // Print the state of all inputs as a single string of ones and zeros
+    String inputStates = "";
+    for (int i = 0; i < 4 * 16; i++) {
+        inputStates += mcpManager.getInputState(i) ? "1" : "0";
     }
-    Serial.println(" ");
+    Serial.println("Input: " + inputStates);
 
     delay(1000);
 }
